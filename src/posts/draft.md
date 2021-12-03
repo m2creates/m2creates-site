cd0c68913553794fb74a38d85684ed02868f5057
Melanie E Magdalena <hello@melaniemagdalena.com>
2021-12-03 01:20:49 -0600

    feat(posts): support drafts

---
+++ src/posts/draft.md
@@ -0,0 +1,18 @@
+---
+title: Draft Post
+slug: draft-post
+date: Last Modified
+datePublish: 2021-12-05
+draft: true
+# seo:
+#   title:
+#   desc:
+#   image_card:
+#   image_alt:
+---
+
+{{ title }}
+
+I am a sample post.
+
+{{ datePublish | readableDate }}
---
+++ src/posts/posts.11tydata.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const isDevEnv = process.env.ELEVENTY_ENV !== 'production';
+const todaysDate = new Date();
+
+function showDraft(data) {
+    const isDraft = 'draft' in data && data.draft !== false;
+    const isFutureDate = data.page.datePublish > todaysDate;
+    return isDevEnv || (!isDraft && !isFutureDate);
+}
+
+module.exports = function() {
+    return {
+        eleventyComputed: {
+            eleventyExcludeFromCollections: function(data) {
+                if(showDraft(data)) {
+                    return data.eleventyExcludeFromCollections;
+                }
+                else {
+                    return true;
+                }
+            },
+            permalink: function(data) {
+                if(showDraft(data)) {
+                    return data.permalink
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+    }
+}
