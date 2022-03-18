d028a8cc8cbc7342b15db1cf22743c21310fdb21
Melanie E Magdalena <hello@melaniemagdalena.com>
2022-03-17 22:59:26 -0500

    update feeds

--- src/config/filters.js
+++ src/config/filters.js
@@ -6,68 +6,78 @@
 const { DateTime } = require('luxon');
 
 module.exports = {
-  // Debug
-  debug: obj => {
-    return util.inspect(obj)
-  },
+	// Debug
+	debug: (obj) => {
+		return util.inspect(obj);
+	},
 
-  // Dates via Luxon
-  /**
-   * Format a date. See readableDate for example.
-   */
-  format: function(date, format) {
-    return DateTime.fromJSDate(date).toFormat(String(format))
-  },
+	// Dates via Luxon
+	/**
+	 * Format a date. See readableDate for example.
+	 */
+	format: function (date, format) {
+		return DateTime.fromJSDate(date).toFormat(String(format));
+	},
 
-  /**
-   * Get date in ISO
-   */
-  iso: function(date) {
-    return DateTime.fromJSDate(date).toISO({
-      includeOffset: false,
-      suppressMilliseconds: true
-    })
-  },
-  
-  /**
-   * Convert Date from ISO
-   */
-  fromIso: function(timestamp) {
-    return DateTime.fromISO(timestamp, { zone: 'America/Chicago' }).toJSDate();
-  },
+	/**
+	 * Get date in ISO
+	 */
+	iso: function (date) {
+		return DateTime.fromJSDate(date).toISO({
+			includeOffset: false,
+			suppressMilliseconds: true,
+		});
+	},
 
-  /**
-   * Readable Date
-   * Default: 'dd MMMM yyyy' in 'America/Chicago'
-   *
-   * Simple usage, like on a post:
-   * date | readableDate
-   *
-   * Use with format, like in Sitemap:
-   * item.date | readableDate('yyyy-LL-dd')
-   */
-  readableDate: function(date, format) {
-    // default to America/Chicago Timezone
-    if (!format) {
-      format = 'dd MMMM yyyy';
-    }
-    return DateTime.fromJSDate(date, { zone: 'America/Chicago' }).toFormat(format);
-  },
+	/**
+	 * Convert Date from ISO
+	 */
+	fromIso: function (timestamp) {
+		return DateTime.fromISO(timestamp, { zone: 'America/Chicago' }).toJSDate();
+	},
 
+	/**
+	 * Readable Date
+	 * Default: 'dd MMMM yyyy' in 'America/Chicago'
+	 *
+	 * Simple usage, like on a post:
+	 * date | readableDate
+	 *
+	 * Use with format, like in Sitemap:
+	 * item.date | readableDate('yyyy-LL-dd')
+	 */
+	readableDate: function (date, format) {
+		// default to America/Chicago Timezone
+		if (!format) {
+			format = 'dd MMMM yyyy';
+		}
+		return DateTime.fromJSDate(date, { zone: 'America/Chicago' }).toFormat(
+			format
+		);
+	},
 
-  /**
-   * Convert Date to ISO
-   * Used in meta for published time
-   */
-  toIso: function(dateObj) {
-    return DateTime.fromJSDate(dateObj).toISO({ includeOffset: true, suppressMilliseconds: true});
-  },
+	/**
+	 * Convert Date to ISO
+	 * Used in meta for published time
+	 */
+	toIso: function (dateObj) {
+		return DateTime.fromJSDate(dateObj).toISO({
+			includeOffset: true,
+			suppressMilliseconds: true,
+		});
+	},
 
-  /**
-   * For RSS Feed
-   */
-  toRFC2822: function(date) {
-    return DateTime.fromJSDate(date).toRFC2822();
-  },
+	/**
+	 * For RSS Feed
+	 */
+	toRFC2822: function (date) {
+		return DateTime.fromJSDate(date).toRFC2822();
+	},
 
-}
+	/**
+	 * For Atom Feed
+	 */
+	toRFC3339: function (date) {
+		return DateTime.fromJSDate(date).toISO();
+	},
+};
