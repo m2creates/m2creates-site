f166e886edc891bde6de6f942d5397d3ca3422c6
Melanie E Magdalena <hello@melaniemagdalena.com>
2022-03-17 22:58:49 -0500

    add scripts

--- src/config/passthroughs.js
+++ src/config/passthroughs.js
@@ -4,20 +4,23 @@
  */
 
 module.exports = {
-  admin: function () {
-    return { 'src/admin': 'admin' }
-  },
+	admin: function () {
+		return { 'src/admin': 'admin' };
+	},
 
-  fonts: function () {
-    return { 'src/assets/fonts': 'assets/fonts' }
-  },
+	fonts: function () {
+		return { 'src/assets/fonts': 'assets/fonts' };
+	},
 
-  images: function () {
-    return { 'src/assets/images': 'images' }
-  },
+	images: function () {
+		return { 'src/assets/images': 'images' };
+	},
 
-  redirects: function () {
-    return { 'src/generate/_redirects': '_redirects' }
-  }
+	redirects: function () {
+		return { 'src/generate/_redirects': '_redirects' };
+	},
 
-}
+	scripts: function () {
+		return { 'src/assets/scripts': 'assets/scripts' };
+	},
+};
