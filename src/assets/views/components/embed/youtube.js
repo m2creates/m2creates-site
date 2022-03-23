66c062b975859447bb2d4fccea09ffc973443bfb
Melanie E Magdalena <hello@melaniemagdalena.com>
2022-03-22 22:12:25 -0500

    rename YT element

--- src/assets/views/components/embed/youtube.js
+++ src/assets/views/components/embed/youtube.js
@@ -1,14 +1,3 @@
 module.exports = ({ id, title = 'YouTube Video' } = {}) => `
-  <lite-youtube videoid="${id}" style="background-image: url('https://i.ytimg.com/vi/${id}/hqdefault.jpg');"><a href="https://youtube-nocookie.com/watch?v=${id}" class="lty-playbtn" title="Play YouTube Video"><span class="lyt-visually-hidden">Play: ${title}</span></a></lite-youtube>
+  <youtube-embed videoid="${id}" style="background-image: url('https://i.ytimg.com/vi/${id}/hqdefault.jpg');"><a href="https://youtube-nocookie.com/watch?v=${id}" class="lty-playbtn" title="Play YouTube Video"><span class="visually-hidden">Play: ${title}</span></a></youtube-embed>
 `;
-
-/**
- * Shortcode returns [object Object]...
- * can something be learned from https://github.com/gfscott/eleventy-plugin-youtube-embed/blob/main/lib/buildEmbed.js
- * 
- * draft:
-{% YouTube {
-  id: 'ULoXTeb3gkQ',
-  title: 'Using Airtable as an Analytics Hub: LinkedIn with Web Clipper & Automations - Update Post Stats'
-} %}
-*/
