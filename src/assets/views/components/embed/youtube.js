274d52df3f06090ad008195a0302620ce1cff674
Melanie E Magdalena <hello@melaniemagdalena.com>
2022-03-24 05:46:15 -0500

    add videotitle for iframe

--- src/assets/views/components/embed/youtube.js
+++ src/assets/views/components/embed/youtube.js
@@ -1,3 +1,3 @@
 module.exports = ({ id, title = 'YouTube Video' } = {}) => `
-  <youtube-embed videoid="${id}" style="background-image: url('https://i.ytimg.com/vi/${id}/hqdefault.jpg');"><a href="https://youtube-nocookie.com/watch?v=${id}" class="lty-playbtn" title="Play YouTube Video"><span class="visually-hidden">Play: ${title}</span></a></youtube-embed>
+  <youtube-embed videoid="${id}" videotitle="${title}" style="background-image: url('https://i.ytimg.com/vi/${id}/hqdefault.jpg');"><a href="https://youtube-nocookie.com/watch?v=${id}" class="lty-playbtn"><span class="visually-hidden">Play: ${title}</span></a></youtube-embed>
 `;
