ef61345e9b889fef1142519fed70a5d83f3379e2
Melanie E Magdalena <hello@melaniemagdalena.com>
2022-03-24 03:43:48 -0500

    add next/prev post support

--- src/config/collections.js
+++ src/config/collections.js
@@ -5,10 +5,20 @@
 
 module.exports = {
 	posts: function (collection) {
-		return collection.getFilteredByGlob('src/posts/*.md').sort(function(a, b) {
-      return b.data.datePublish - a.data.datePublish; // sort by date - descending
-      //return a.data.datePublish - b.data.datePublish; // sort by date - ascending
-    });
+		const post = collection.getFilteredByGlob('src/posts/*.md');
+
+		for (let i = 0; i < post.length; i++) {
+			const prevPost = post[i - 1];
+			const nextPost = post[i + 1];
+
+			post[i].data['prevPost'] = prevPost;
+			post[i].data['nextPost'] = nextPost;
+		}
+
+		return post.sort(function (a, b) {
+			return b.data.datePublish - a.data.datePublish; // sort by date - descending
+			//return a.data.datePublish - b.data.datePublish; // sort by date - ascending
+		});
 	},
 
 	pages: function (collection) {
