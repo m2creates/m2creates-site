944d64bc9625bdba44c487717f6d2fdb5f33528d
Melanie E Magdalena <hello@melaniemagdalena.com>
2022-09-23 00:21:05 +0000

    Update twitch-embed.js and twitch.js

--- src/assets/views/components/embed/twitch.js
+++ src/assets/views/components/embed/twitch.js
@@ -7,6 +7,24 @@
  * - Collection https://player.twitch.tv/?collection=<id>&parent=<website.com>
  */
 
-module.exports = ({ id, title = 'Twitch Video', type, thumbnail } = {}) => `
-  <twitch-embed videoid="${id}" videotitle="${title}" videotype="${type}" class="twitch-playbtn" style="${ thumbnail && `background-image:url('/images/${thumbnail}')`};">${(() => {if (type === 'clip') {return `<a href="https://clips.twitch.tv/embed?clip=${id}">`} else if (type === 'collection') {return `<a href="https://player.twitch.tv/?collection=${id}">`} else {return `<a href="https://player.twitch.tv/?video=${id}">`}})()}<span class="visually-hidden">Play ${title}</span></a></twitch-embed>
+module.exports = ({ id, title = "Twitch Video", type, thumbnail } = {}) => `
+  <twitch-embed
+    videoid="${id}"
+    videotitle="${title}"
+    videotype="${type}"
+    class="twitch-playbtn" style="${
+      thumbnail && `background-image:url('/images/${thumbnail}')`
+    };">
+        ${(() => {
+          if (type === "clip") {
+            return `<a href="https://clips.twitch.tv/${id}">`;
+          } else if (type === "collection") {
+            return `<a href="https://www.twitch.tv/collections/${id}">`;
+          } else {
+            return `<a href="https://www.twitch.tv/videos/${id}">`;
+          }
+        })()}
+        <span class="visually-hidden">Play ${title}</span>
+        </a>
+</twitch-embed>
 `;
