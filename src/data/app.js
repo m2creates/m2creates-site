40f3b720b22e6a57a76fcca7469e3cb03c2d3cff
Melanie E Magdalena <hello@melaniemagdalena.com>
2022-11-04 16:12:50 -0500

    revert git Last Modified

--- src/data/app.js
+++ src/data/app.js
@@ -1,7 +1,4 @@
 module.exports = {
   environment: process.env.ELEVENTY_ENV,
-  now: new Date(),
-  data: if(process.env.NODE_ENV === "production") {
-      data.date = "git Last Modified";
-  }
+  now: new Date()
 };
