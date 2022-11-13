f2cfe795fa026ca35dbcd4e85af6eb543020de91
Melanie E Magdalena <hello@melaniemagdalena.com>
2022-11-12 19:09:25 -0600

    fix(shortcode): add title

--- src/assets/views/components/callout/uplabs.js
+++ src/assets/views/components/callout/uplabs.js
@@ -1,6 +1,6 @@
-module.exports = ({ prompt, challengers, results, submission } = {}) => `
+module.exports = ({ title, prompt, challengers, results, submission } = {}) => `
   <div class="not-prose callout flex flex-col items-center justify-between gap-4 w-full px-5 py-4 my-8 text-white no-underline bg-purple-600">
-    <p class="w-full text-center"><span class="font-bold uppercase text-sm">Prompt:</span><br><span class="text-2xl">“<em>${prompt}</em>”</span></p>
+    <p class="w-full text-center"><span class="font-bold uppercase text-sm">${title}</span><br><span class="text-2xl">“<em>${prompt}</em>”</span></p>
     <div class="flex flex-row justify-between grow shrink-0 gap-4">
       <a href="${results}" target="_blank" class="border-2 border-white hover:bg-white px-4 py-2 font-bold">Results (${challengers} challengers)</a>
       <a href="${submission}" target="_blank" class="border-2 border-white hover:bg-white px-4 py-2 font-bold">My Submission</a>
