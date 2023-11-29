9b5297972443ba892160d64898396ede4f3df0d0
Melanie E Magdalena <hello@melaniemagdalena.com>
2023-11-28 19:39:10 -0600

    update dependencies / remove storybook (#32)
    * update deps
    * remove storybook
    * rename css:prod
    * fix list layouts, nav, header spacing

--- postcss.config.js
+++ postcss.config.js
@@ -1,27 +1,3 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-	// Specify the paths to all of the template files in your project
-	content: ['./dist/*.html', './dist/**/*.html'],
-	whitelist: [
-		'body',
-		'html',
-		'h1',
-		'h2',
-		'h3',
-		'p',
-		'a',
-		'blockquote',
-		'.dark',
-		'object-cover',
-		'object-contain',
-		'object-fill',
-		'object-scale-down',
-		'object-none',
-		'visually-hidden',
-	],
-	// Include any special characters you're using in this regular expression
-	defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 const cssnano = require('cssnano')({
   preset: ['default', {
     discardComments: {
@@ -34,6 +10,6 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...process.env.TAILWIND_ENV === 'production' ? [purgecss,  cssnano] : []
+    ...process.env.TAILWIND_ENV === 'production' ? [cssnano] : []
   ],
 };
